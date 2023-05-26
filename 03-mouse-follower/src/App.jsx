53eae95bc2e043158afebc74c5fd594a6e975c09
seba0714 <daniortiz0714@gmail.com>
2023-05-25 22:34:21 -0300

    nuevo proyecto

--- 03-mouse-follower/src/App.jsx
+++ 03-mouse-follower/src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
+const FollowMouse =() =>{
+const [enabled, setEnabled] = useState(false)
+const [position,setPosition] = useState({x:0, y:0})
+useEffect(() => {
+  console.log('efecto', { enabled })
+
+  const handleMove=(event) =>{
+    const {clientX, clientY} = event
+    console.log('handleMove',{clientX, clientY})
+    setPosition({x:clientX, y:clientY})
+  }
+  if(enabled){
+  window.addEventListener('pointermove',handleMove)
+  }
+  // clean up se ejecuta cuando el componente se desmonta y cuando cambian las dependencias.
+ return() =>{
+  window.removeEventListener('pointermove',handleMove)
+ }
+}, [enabled])
+
+
+return (
+  <>
+    <main>
+    <div style={{
+      position: 'absolute',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      border: '1px solid #fff',
+      borderRadius: '50%',
+      opacity: 0.8,
+      pointerEvents: 'none',
+      left: -25,
+      top: -25,
+      width: 50,
+      height: 50,
+      transform: `translate(${position.x}px,${position.y}px)`
+    }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir pointer
+      </button>
+    </main>
+  </>
+)
+  }
+
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  return(
+    <main>
+    <FollowMouse />
+    </main>
   )
-}
 
+}
+  
 export default App
